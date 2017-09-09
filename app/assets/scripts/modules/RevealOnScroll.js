98b8cb2562a043d0575eccdd81fc43fd7b0ed0c5
Tia <msksfo@gmail.com>
2017-09-09 13:49:38 -0700

    finished the lecture, but this is still not working

--- app/assets/scripts/modules/RevealOnScroll.js
+++ app/assets/scripts/modules/RevealOnScroll.js
@@ -1,11 +1,15 @@
 // in order to use jQuery in this module, we need to import it first.
 import $ from "jquery"; // this will automatically work as long as jquery is in node modules folder
 
+// the waypoints package does not have a 'main' file. so we need to manually point to the node modules folder and specify the exact file we want
+import waypoints from '../../../../node_modules/waypoints/lib/noframework.waypoints';
+
 class RevealOnScroll {
 	constructor() {
-		// select the items to reveal when scrolled to
+		// select the items to reveal when scrolled to. this is a collection of elements with references to the four dom elements on the page that have the class of feature-item
 		this.itemsToReveal = $(".feature-item");
 		this.hideInitially(); // we want this to run on page load
+		this.createWaypoints(); // we want this function to run on page load
 	}
 
 	hideInitially(){
@@ -13,6 +17,30 @@ class RevealOnScroll {
 		//reference the property that points to the elements
 		this.itemsToReveal.addClass("reveal-item");
 	}
+
+	createWaypoints(){
+		this.itemsToReveal.each(function(){
+			var currentItem = this;
+			new Waypoint({
+				element: currentItem,  // the dom element to watch for as we scroll down
+				handler: function(){ // what we want to happen when the element is scrolled to --> add the css modifyer so that it gradually becomes visible
+					$(currentItem).addClass('reveal-item--is-visible');
+				},
+				offset: '85%'
+			};
+		});
+	}
 }
 
 export default RevealOnScroll;
+
+
+
+
+
+
+
+
+
+
+
