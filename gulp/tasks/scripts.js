19fb18c890430781e4e22bb43aa353beea48d2d4
Tia <msksfo@gmail.com>
2017-09-13 08:53:52 -0700

    account for older browsers that do not support svg files

--- gulp/tasks/scripts.js
+++ gulp/tasks/scripts.js
@@ -1,7 +1,7 @@
 var gulp = require('gulp');
 var webpack = require('webpack');
 
-gulp.task('scripts', function(callback){
+gulp.task('scripts', ['modernizr'], function(callback){
 	//run webpack programatically from within a file.
 	webpack(require('../../webpack.config.js'), function(err, stats){
 		if (err){
